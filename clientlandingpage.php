f67ae7bf84d49147303cab21a15bbab81c2b027b
todosantana <todosantana@gmail.com>
2021-03-21 23:11:40 +0700

    First commit client landing page

---
+++ clientlandingpage.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+    <title>U-Counsel</title>
+    <!-- LOGO -->
+    <link rel = "icon" href="photos/logo.png" type = "image/x-icon">
+    <!-- LOGO -->
+
+    <!-- STYLING -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- STYLING -->
+</head>
+
+<body>
+
+
+    <!-- TITLE -->
+    <div class="content center" style="margin-top:50px; width:1000px;">
+        <h2 style="color:#7D7E80;">Hi, James! How can we help you today? </h2>
+    </div>
+    <!-- TITLE -->
+
+    <!-- CONTENT -->
+    <table class="center" style="width:1200px; margin-top:100px; margin-left:180px;">
+        <tr>
+            <td style="width:400px;">
+
+            <div class="card shadow p-3" style="width: 300px;">
+                <img class="card-img-top" src="PHOTOS/journal2.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title" style="text-align:center; color:#0788D9;">My Journal</h4>
+                    <p class="card-text" style="font-size:20px;">Help us to understand you each and every day.</p>
+                    <a href="myjournal1.php" style="text-decoration: none;"><button type="submit" class="btn btn-outline-primary btn-sm">Open My Journal</button></a>
+            </div>
+            </div>
+            </td>
+            <td>
+            <div class="card shadow p-3" style="width: 300px;">
+                <img class="card-img-top" src="PHOTOS/appointment.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title" style="text-align:center; color:#0788D9;">Schedule an Appointment</h4>
+                    <p class="card-text" style="font-size:20px;">Connect with us and feel free to talk about anything to us!</p>
+                    <a href="schedule1.php" style="text-decoration: none;"><button type="submit" class="btn btn-outline-primary btn-sm">Create Schedule</button></a>
+            </div>
+            </div>
+            </td>
+            <td>
+            <div class="card shadow p-3" style="width: 300px;">
+                <img class="card-img-top" src="PHOTOS/message.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title" style="text-align:center; color:#0788D9;">Leave us a Message</h4>
+                    <p class="card-text" style="font-size:20px;">Send us your thoughts because every mind should be free!</p>
+                    <a href="message.php" style="text-decoration: none;"><button type="submit" class="btn btn-outline-primary btn-sm">Send Message</button></a>
+            </div>
+            </div>
+            </td>
+        </tr>
+    </table>
+    <!-- CONTENT -->
+
+</body>
+</html>
