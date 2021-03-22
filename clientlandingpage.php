991749b7eaf444713aafa7b520a08fdf392f8393
alyaarifahrizqy <alyarifahrz@gmail.com>
2021-03-22 10:58:15 +0700

    landing page user

--- clientlandingpage.php
+++ clientlandingpage.php
@@ -1,8 +1,8 @@
 <html>
 <head>
-    <title>U-Counsel</title>
+    <title>You-Counsel</title>
     <!-- LOGO -->
-    <link rel = "icon" href="photos/logo.png" type = "image/x-icon">
+    <link rel = "icon" href="photos/logoo.png" type = "image/x-icon">
     <!-- LOGO -->
 
     <!-- STYLING -->
@@ -10,6 +10,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- STYLING -->
 </head>
 
@@ -17,8 +18,8 @@
 
 
     <!-- TITLE -->
-    <div class="content center" style="margin-top:50px; width:1000px;">
-        <h2 style="color:#7D7E80;">Hi, James! How can we help you today? </h2>
+    <div class="content center" style="margin-top:60px; width:100%;">
+        <h2 style="color:#7D7E80; text-align: center; font-size: 40px;">Hi, James! How can we help you today? </h2>
     </div>
     <!-- TITLE -->
 
@@ -31,8 +32,8 @@
                 <img class="card-img-top" src="PHOTOS/journal2.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h4 class="card-title" style="text-align:center; color:#0788D9;">My Journal</h4>
-                    <p class="card-text" style="font-size:20px;">Help us to understand you each and every day.</p>
-                    <a href="myjournal1.php" style="text-decoration: none;"><button type="submit" class="btn btn-outline-primary btn-sm">Open My Journal</button></a>
+                    <p class="card-text" style="font-size:20px;text-align: center;">Help us to understand you each and every day.</p>
+                    <center><a href="myjournal1.php" style="text-decoration: none;"><button type="submit" class="btn btn-outline-primary btn-sm">Open My Journal</button></a></center>
             </div>
             </div>
             </td>
@@ -41,8 +42,8 @@
                 <img class="card-img-top" src="PHOTOS/appointment.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h4 class="card-title" style="text-align:center; color:#0788D9;">Schedule an Appointment</h4>
-                    <p class="card-text" style="font-size:20px;">Connect with us and feel free to talk about anything to us!</p>
-                    <a href="schedule1.php" style="text-decoration: none;"><button type="submit" class="btn btn-outline-primary btn-sm">Create Schedule</button></a>
+                    <p class="card-text" style="font-size:20px;text-align: center;">Connect with us and feel free to talk about anything to us!</p>
+                    <center><a href="schedule1.php" style="text-decoration: none;"><button type="submit" class="btn btn-outline-primary btn-sm">Create Schedule</button></a></center>
             </div>
             </div>
             </td>
@@ -51,13 +52,21 @@
                 <img class="card-img-top" src="PHOTOS/message.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h4 class="card-title" style="text-align:center; color:#0788D9;">Leave us a Message</h4>
-                    <p class="card-text" style="font-size:20px;">Send us your thoughts because every mind should be free!</p>
-                    <a href="message.php" style="text-decoration: none;"><button type="submit" class="btn btn-outline-primary btn-sm">Send Message</button></a>
+                    <p class="card-text" style="font-size:20px;text-align: center">Send us your thoughts because every mind should be free!</p>
+                    <center><a href="message.php" style="text-decoration: none;"><button type="submit" class="btn btn-outline-primary btn-sm">Send Message</button></a></center>
             </div>
             </div>
             </td>
         </tr>
     </table>
+
+    <!-- SOCMED -->
+    
+    <a href="#" class="fa fa-twitter fa-3x" style="color: blue; margin-left: 40%; margin-top: 40px;"></a>
+    <a href="#" class="fa fa-facebook fa-3x" style="color: blue; margin-left: 20px;"></a>
+    <a href="#" class="fa fa-instagram fa-3x" style="color: blue; margin-left: 20px;"></a>
+    <a href="#" class="fa fa-google fa-3x" style="color: blue; margin-left: 20px;"></a>
+
     <!-- CONTENT -->
 
 </body>
