024e54c5f76ebe89c19921ca1345feff71233ae3
alyaarifahrizqy <alyarifahrz@gmail.com>
2021-06-23 19:06:56 +0700

    resolve #48

---
+++ tests/Browser/ClientLoginTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ClientLoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     * @group clientlogin
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('http://127.0.0.1:8000/login')
+                    ->assertSee('Welcome back!')
+                    ->type('username','alexgundara')
+                    ->type('password','12345')
+                    ->press('LOGIN NOW');
+        });
+    }
+}
