c9790f4e352ef1f182b2c5e7c6565401e1230431
Aria Fajar Ramdhany <aria.fajar14@gmail.com>
2021-06-24 16:39:52 +0700

    ok

--- resources/views/counselor_appointment_reply.blade.php
+++ resources/views/counselor_appointment_reply.blade.php
@@ -58,7 +58,7 @@
                 </li>
             </ul>
             <span class="navbar-text" style="color:white;">
-                <a href="/profile"><button type="button" class="btn btn-sm btn-outline-light">Profile</button></a>
+                <a href="/counselorprofile"><button type="button" class="btn btn-sm btn-outline-light">Profile</button></a>
                 <a href="/logout"><button type="button" class="btn btn-sm btn-outline-light">Logout</button></a>
             </span>
         </div>
--- tests/Browser/JournalCekTest.php
+++ tests/Browser/JournalCekTest.php
@@ -16,10 +16,20 @@ class JournalCekTest extends DuskTestCase
     public function testExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('http://127.0.0.1:8000/counselor_journal_checker')
-                    ->assertSee('Journal Checker')
-                    ->type('username','user')
+            $browser->visit('http://127.0.0.1:8000/login')
+                    ->assertSee('Welcome back!')
+                    ->type('username', 'skinner123')
+                    ->type('password', '12345')
+                    ->press('LOGIN NOW')    
+                    ->assertSee('Counselor Page')
+                    ->press('Go to Journal Checker')
+                    ->type('username', 'aria')
                     ->press('Submit');
+
+                  //  $browser->visit('http://127.0.0.1:8000/counselor_journal_checker')
+                  //   ->assertSee('Journal Checker')
+                  //  ->type('username','user')
+                  //  ->press('Submit');
         });
     }
 }
